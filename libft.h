ba737ba035375865c9b89dae8011d20c7e7c3760
Diederik Robert marie boeren <drobert-@c1r18s3.42lisboa.com>
2022-02-15 11:19:45 +0000

    ADD strchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: drobert- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 11:32:31 by drobert-          #+#    #+#             */
-/*   Updated: 2022/02/15 11:07:08 by drobert-         ###   ########.fr       */
+/*   Updated: 2022/02/15 11:17:43 by drobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t dstsize);
 void	*ft_memset(void *dst, int c, size_t dstsize);
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 #endif
