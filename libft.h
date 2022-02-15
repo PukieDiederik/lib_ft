0bab9c7cf30f9487d1074afa9f33a3059844f2b3
Diederik Robert marie boeren <drobert-@c1r18s3.42lisboa.com>
2022-02-15 13:26:55 +0000

    ADD strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: drobert- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 11:32:31 by drobert-          #+#    #+#             */
-/*   Updated: 2022/02/15 11:30:55 by drobert-         ###   ########.fr       */
+/*   Updated: 2022/02/15 13:12:02 by drobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ void	ft_bzero(void *s, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
